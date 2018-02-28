06bd6a6bc7716c696e35dc0816e881fbbbbf6ee3
582302894 <582302894@qq.com>
2018-02-28 22:48:22 +0800

    2018-02-28 22:48:10

---
+++ 1-50/ex29_if.py
@@ -0,0 +1,30 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print "Too many cats!The world is doomed!"
+
+if people > cats:
+    print "Not many cats!The world is saved!"
+
+if people < dogs:
+    print "The world is drooled on!"
+
+if people > dogs:
+    print "The world is dry!"
+
+dogs += 5
+
+print dogs
+
+
+if people > cats:
+    print "if people gt cats"
+else:
+    print "if people lt cats"
+
+if cats < people:
+    print "if cats lt people"
+elif cats > people:
+    print "elif cats gt people"
